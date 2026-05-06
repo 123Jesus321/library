8b191a77c02399a4fbaeacb2f73c77d494387ee8
library <matter2131a@gmail.com>
2026-05-06 16:16:58 +0300

    Update books.js

--- books.js
+++ books.js
@@ -88,7 +88,7 @@ const books = [
         id: "Виртуаль. Мир Хранителей",
         title: "Виртуаль. Мир Хранителей",
         author: "Вистериона",
-        pdf: "VirtualWorldOfGuard (2).pdf",
+        pdf: "VirtualWorldOfGuard.pdf",
         seriesId: "Virtual-ras",
         seriesTitle: "Виртуаль",
         seriesOrder: 1,
